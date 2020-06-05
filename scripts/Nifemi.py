74fe900b7421a04241934c14da1e1fc382417c27
Nifemiii <faithfuloladipo@gmail.com>
2020-06-05 19:14:20 +0100

    add my script

---
+++ scripts/Nifemi.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 17:45:46 2020
+
+@author: Marvelous
+"""
+
+print ("Hello World, this is oladipo faithful with HNGi7 ID HNG-01768 and email faithfuloladipo@gmail.com using python for stage 2 task")
